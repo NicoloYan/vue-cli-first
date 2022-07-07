0fd886b71b396e8c6bbcfa5a3d643bbf69c27741
NicoloYan <nico.yan@outlook.it>
2022-07-07 12:29:52 +0200

    Excercise complete

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <PageTitle />
+    <MyHero />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import PageTitle from './components/PageTitle.vue';
+import MyHero from './components/MyHero.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    PageTitle,
+    MyHero
   }
 }
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/MyHero.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <img src="https://www.nieddittas.it/wp-content/uploads/2021/12/qual-e-il-mare-piu-bello-della-sardegna.jpg" alt="mare">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyHero'
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+    height: 500px;
+    width: 100%;
+    margin-top: 30px;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+}
+</style>
